5e9ec7522db9900e9fa4ef65ff66ca26eb67c6fa
t.nastase@student.utwente.nl <HTTzKh9CYn>
2022-01-24 14:44:20 +0100

    Initial commit

---
+++ venv/current_system.py
@@ -0,0 +1,13 @@
+import csv
+
+dictlist =[]
+
+with open('Votes2.csv', mode='r') as inp:
+    reader = csv.DictReader(inp)
+
+    for row in reader:
+        dictlist.append(row)
+
+
+for line in dictlist:
+    print(line['State'])
